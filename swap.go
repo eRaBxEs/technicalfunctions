22af38429a86420a019be196bb720f6493220841
eRaBxEs <ehiojee@yahoo.com>
2019-01-16 18:14:10 +0100

    add comments to called function

--- swap.go
+++ swap.go
@@ -12,7 +12,12 @@ func main() {
 
 	fmt.Println("Initial arrangement of d:", d)
 
+	// Need to ensure that we use a seed that is constantly changing e.g time
+	// and that is what enables us to get a different sequence of random number
+
 	source := rand.NewSource(time.Now().UnixNano())
+
+	//After obtaining a source of type Source, get a Rand by using func New from package rand
 	r := rand.New(source)
 
 	for i := range d {
